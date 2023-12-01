6606e37384db7c4b4f7811809dd133f46d4072d9
Alifuatakcn <fakcan4908@gmail.com>
2023-12-01 14:52:59 +0300

    text

---
+++ myapp/components/flex1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const flex1 = () => {
+  return (
+    <View>
+      <Text>flex1</Text>
+    </View>
+  )
+}
+
+export default flex1
+
+const styles = StyleSheet.create({})
