12b7f7aea2e5ea86e05a55863a593cac0feb63ef
Alifuatakcn <fakcan4908@gmail.com>
2023-12-06 20:29:07 +0300

    v

---
+++ myapp/components/flex3.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Flex3 = () => {
+  return (
+    <View>
+      <Text>Flex3</Text>
+    </View>
+  )
+}
+
+export default Flex3
+
+const styles = StyleSheet.create({})
