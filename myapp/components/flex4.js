315d2c69146c427db4a923bcf1cb271c4e607782
Alifuatakcn <fakcan4908@gmail.com>
2024-01-08 20:06:55 +0300

    v4

---
+++ myapp/components/flex4.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class flex4 extends Component {
+  render() {
+    return (
+      <View>
+        <Text>flex4</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({})
